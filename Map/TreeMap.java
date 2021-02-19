cec089fe954a23f44cc74cb03eb175eb599f2c18
HrlCoder <634464975@qq.com>
2021-02-19 17:03:49 +0800

    Map

---
+++ Map/TreeMap.java
@@ -0,0 +1,26 @@
+package JavaSE4.Map;
+
+/**
+ * @author ：浪漫不死
+ * @version:
+ * @description：
+ * @date : 2020/10/29 19:15
+ */
+public class TreeMap {
+    private TreeNode root;
+
+//    public Integer put(String key,Integer value) {
+//        if(root == null) {
+//            root = new TreeNode(key,value);
+//            return null;
+//        }
+//    }
+
+//    public Integer get(String key) {
+//
+//    }
+//
+//    public Integer getOrDefault(String key,Integer getdefault) {
+//
+//    }
+}
