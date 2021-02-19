d41217f3b45dcecb7f80084963ba41b56318b229
HrlCoder <634464975@qq.com>
2021-02-19 17:05:15 +0800

    Map

---
+++ Map/TreeNode.java
@@ -0,0 +1,20 @@
+package JavaSE4.Map;
+
+/**
+ * @author ：浪漫不死
+ * @version:
+ * @description：
+ * @date : 2020/10/29 19:24
+ */
+public class TreeNode {
+    String key;
+    Integer value;
+
+    TreeNode right;
+    TreeNode left;
+
+    public TreeNode(String key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+}
